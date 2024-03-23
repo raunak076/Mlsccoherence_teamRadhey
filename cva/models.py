291f686c9a304ac5de931914599c529330a3423a
Dishant Shah <dishantshah3133@gmail.com>
2024-03-23 23:36:46 +0530

    Db Model creation + Hugging Face Model

--- cva/models.py
+++ cva/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
+    password = models.CharField(max_length=128)
+    def __str__(self):
+        return self.name
+    
+class context(models.Model):
+    email = models.CharField(max_length=128)
+    contextText = models.CharField(max_length=1024)
+    def __str__(self):
+        return self.email+"-"+self.contextText
+
--- cva/views.py
+++ cva/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json, requests
+# Load model directly
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-API_URL = "https://api-inference.huggingface.co/models/SantiagoPG/chatbot_customer_service"
-headers = {"Authorization": "Bearer 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"}
+# tokenizer = AutoTokenizer.from_pretrained("SantiagoPG/chatbot_customer_service")
+# model = AutoModelForSeq2SeqLM.from_pretrained("SantiagoPG/chatbot_customer_service")
+
+from transformers import BartForConditionalGeneration, BartTokenizer,BartForConditionalGeneration
+
+model = BartForConditionalGeneration.from_pretrained("SantiagoPG/chatbot_customer_service", forced_bos_token_id=0)
+tok = BartTokenizer.from_pretrained("facebook/bart-large")
+
+# API_URL = "https://api-inference.huggingface.co/models/SantiagoPG/chatbot_customer_service"
+# headers = {"Authorization": "Bearer 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"}
 
 def index(request):
     print("hi")
@@ -19,19 +29,23 @@ def query(payload):
 
 @csrf_exempt
 def processText(request):
-    # if request.method == "POST":
-    #     print(request.body)
-    #     json_data = request.body.decode('utf-8')
-    #     data = json.loads(json_data)
-    #     query = data.get("text")
-    #     print(query)
-        
-    #     try:
-    #         response = requests.post(API_URL, headers=headers, json={"inputs":query})
-    #         output = response.json()
-    #         return JsonResponse({'message':output})
-    #     except Exception as e:
-    #         return JsonResponse({"Message":"Sorry Unable to understand you"})
-    # else:
-    #     return JsonResponse({'message':'Sorry I am unable to understand you'})
+    if request.method == "POST":
+        print(request.body)
+        json_data = request.body.decode('utf-8')
+        data = json.loads(json_data)
+        query = data.get("text")
+        print(query)
+        try:
+            #!
+            encoded_query = tok.encode(query, return_tensors="pt")            
+            inputs = {"inputs": encoded_query}
+            generated_output = model.generate(**inputs)
+            decoded_text = tok.batch_decode(generated_output, skip_special_tokens=True)[0]
+            # print(f"Generated Text: {decoded_text}")
+            return JsonResponse({'message':"Kaise ho Roshan babua"})
+            # return HttpResponse("hi")
+        except Exception as e:
+            return JsonResponse({"Error":e})
+    else:
+        return JsonResponse({'message':'Sorry I am unable to understand you'})
     return JsonResponse({'Message':'Hello'})
